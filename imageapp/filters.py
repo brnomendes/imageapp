cab8f92c58ea27da67205233cf163d7e5b4169aa
Bruno Mendes <brunoms.50@gmail.com>
2019-02-07 11:28:31 +0000

    Some modifications to put in README.md

--- imageapp/filters.py
+++ imageapp/filters.py
@@ -2,14 +2,14 @@ from .implementations import blurfilter, rgbdecomposition, puttext
 from .filterregister import register
 
 
-# @register(name='Blur', types=['RGB'], help='Blur Help')
-# def blur(image, radius, weight):
-#     return blurfilter.run(image, int(radius), int(weight))
+@register(name='Blur', types=['RGB'], help='Blur Help')
+def blur(image, radius, weight):
+    return blurfilter.run(image, int(radius), int(weight))
 
 
-# @register(name='RGB Split', types=['RGB'])
-# def rgb(image):
-#     return rgbdecomposition.run(image)
+@register(name='RGB Split', types=['RGB'])
+def rgb(image):
+    return rgbdecomposition.run(image)
 
 
 @register(name='Put Text', types=['RGB'], help='Put text in position x,y in the image')
