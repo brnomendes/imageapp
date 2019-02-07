023bbe1a0ca503f70aaabe8e244055ec2e25529c
Bruno Mendes <brunoms.50@gmail.com>
2019-02-07 11:28:15 +0000

    Some modifications to put in README.md

--- imageapp/filters.py
+++ imageapp/filters.py
@@ -2,16 +2,16 @@ from .implementations import blurfilter, rgbdecomposition, puttext
 from .filterregister import register
 
 
-@register(name='Blur', types=['RGB'], help='Blur Help')
-def blur(image, radius, weight):
-    return blurfilter.run(image, int(radius), int(weight))
+# @register(name='Blur', types=['RGB'], help='Blur Help')
+# def blur(image, radius, weight):
+#     return blurfilter.run(image, int(radius), int(weight))
 
 
-@register(name='RGB Split', types=['RGB'])
-def rgb(image):
-    return rgbdecomposition.run(image)
+# @register(name='RGB Split', types=['RGB'])
+# def rgb(image):
+#     return rgbdecomposition.run(image)
 
 
-@register(name='Put Text', types=['RGB'])
-def text(image, x, y, text, color):
-    return puttext.run(image, int(x), int(y), text, color)
+@register(name='Put Text', types=['RGB'], help='Put text in position x,y in the image')
+def text(image, x, y, text_to_put, color):
+    return puttext.run(image, int(x), int(y), text_to_put, color)
--- imageapp/implementations/puttext.py
+++ imageapp/implementations/puttext.py
@@ -1,8 +1,8 @@
 from PIL import ImageDraw
 
 
-def run(image, x, y, text, color):
+def run(image, x, y, text_to_put, color):
     draw = ImageDraw.Draw(image)
 
-    draw.text((x, y), text, color)
+    draw.text((x, y), text_to_put, color)
     return {'text': image}
