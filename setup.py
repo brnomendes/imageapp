1e16872cd74f886a029d987dc36a5d7425a603b9
Bruno Mendes <brunoms.50@gmail.com>
2019-02-07 11:08:42 +0000

    Remove test command from setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,4 @@
-import sys
-from setuptools.command.test import test as TestCommand
 from setuptools import setup, find_packages
-
-
-class PyTest(TestCommand):
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ""
-
-    def run_tests(self):
-        import shlex
-        import pytest
-
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
-
 
 setup(
     name='imageapp',
