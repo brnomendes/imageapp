63258d08b58aa4bdb48aca41fdc08a97860ad5fc
Bruno Mendes <brunoms.50@gmail.com>
2022-10-23 18:53:52 +0100

    Refactor blurfilter to reduce its Cognitive Complexity

--- app/implementations/blurfilter.py
+++ app/implementations/blurfilter.py
@@ -11,22 +11,29 @@ def convolution(image, original_image, radius, weight):
         for y in range(height):
             pixel = list(image.getpixel((x, y)))
             for c, _ in enumerate(image.getbands()):
-                pixel_sum = 0
-                count = 0
-                for j in range(-radius, radius + 1):
-                    for k in range(-radius, radius + 1):
-                        if _pixel_exists(x + j, y + k, width, height):
-                            value = original_image.getpixel((x + j, y + k))[c]
-                            if j == 0 and k == 0:
-                                pixel_sum += (value * weight)
-                                count = count + weight
-                            else:
-                                pixel_sum += value
-                                count = count + 1
-                pixel[c] = int(pixel_sum / count)
+                pixel[c] = _get_pixel_value(original_image, x, y, radius, weight, width, height, c)
+
             image.putpixel((x, y), tuple(pixel))
 
     return image
+
+
+def _get_pixel_value(
+    original_image, x: int, y: int, radius: int, weight: int, width: int, height: int, channel: int
+) -> int:
+    pixel_sum = 0
+    count = 0
+    for j in range(-radius, radius + 1):
+        for k in range(-radius, radius + 1):
+            if _pixel_exists(x + j, y + k, width, height):
+                value = original_image.getpixel((x + j, y + k))[channel]
+                if j == 0 and k == 0:
+                    pixel_sum += value * weight
+                    count = count + weight
+                else:
+                    pixel_sum += value
+                    count = count + 1
+    return int(pixel_sum / count)
 
 
 def _pixel_exists(x, y, width, height):
